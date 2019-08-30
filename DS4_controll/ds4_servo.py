1628ebbbfe001d3ec01b15ed69f8585576ab2ff0
Ryosuke <rsasaki@keio.jp>
2019-08-30 20:55:06 +0900

    add exit

--- DS4_controll/ds4_servo.py
+++ DS4_controll/ds4_servo.py
@@ -1,6 +1,7 @@
 import pygame
 import pigpio
 import time
+import sys
 
 pygame.init()
 pi=pigpio.pi()
@@ -21,37 +22,40 @@ print(joystick.get_name())
 joystick.init()
 print(joystick.get_numaxes())
 
-while True:
-    # Get the latest events from the system
-    hadEvent = False
-    events = pygame.event.get()
-    # Handle each event individually
-    for event in events:
-        print("event:{}".format(event.type))
-        if event.type == pygame.QUIT:
-            # User exit
-            pass
-        elif event.type == pygame.JOYAXISMOTION:
-            # A joystick has been moved
-            hadEvent = True
-        if hadEvent:
-            leftRight = joystick.get_axis(0)
+try:
+    while True:
+        # Get the latest events from the system
+        hadEvent = False
+        events = pygame.event.get()
+        # Handle each event individually
+        for event in events:
+            print("event:{}".format(event.type))
+            if event.type == pygame.QUIT:
+                # User exit
+                pass
+            elif event.type == pygame.JOYAXISMOTION:
+                # A joystick has been moved
+                hadEvent = True
+            if hadEvent:
+                leftRight = joystick.get_axis(0)
 
-        print(leftRight)
+            print(leftRight)
 
 
-"""
-while True:
-    time.sleep(0.01)
-    print(joystick.get_axis(0), joystick.get_axis(1),
-          joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5))
+    """
+    while True:
+        time.sleep(0.01)
+        print(joystick.get_axis(0), joystick.get_axis(1),
+            joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5))
 
-    updown = joystick.get_axis(0)
-    print(updown)
-    if updown == 0 :
-        pi.set_servo_pulsewidth(4, 1500)
-    elif updown > 0 :
-        pi.set_servo_pulsewidth(4, 2000)
-    elif updown < 0 :
-        pi.set_servo_pulsewidth(4, 1000)
-"""
+        updown = joystick.get_axis(0)
+        print(updown)
+        if updown == 0 :
+            pi.set_servo_pulsewidth(4, 1500)
+        elif updown > 0 :
+            pi.set_servo_pulsewidth(4, 2000)
+        elif updown < 0 :
+            pi.set_servo_pulsewidth(4, 1000)
+    """
+finally:
+    sys.exit()
