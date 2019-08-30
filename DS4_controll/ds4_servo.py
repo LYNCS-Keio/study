a5e084f2edac5c9d82de2f9e7952f83b39d24190
Ryosuke <rsasaki@keio.jp>
2019-08-30 20:47:43 +0900

    copy and paste

--- DS4_controll/ds4_servo.py
+++ DS4_controll/ds4_servo.py
@@ -22,10 +22,30 @@ joystick.init()
 print(joystick.get_numaxes())
 
 while True:
+    # Get the latest events from the system
+    hadEvent = False
+    events = pygame.event.get()
+    # Handle each event individually
+    for event in events:
+        print("event:{}".format(event.type))
+        if event.type == pygame.QUIT:
+            # User exit
+            pass
+        elif event.type == pygame.JOYAXISMOTION:
+            # A joystick has been moved
+            hadEvent = True
+        if hadEvent:
+            leftRight = joystick.get_axis(0)
+
+        print(leftRight)
+
+
+"""
+while True:
     time.sleep(0.01)
     print(joystick.get_axis(0), joystick.get_axis(1),
-          joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5)
-    """
+          joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5))
+
     updown = joystick.get_axis(0)
     print(updown)
     if updown == 0 :
@@ -34,4 +54,4 @@ while True:
         pi.set_servo_pulsewidth(4, 2000)
     elif updown < 0 :
         pi.set_servo_pulsewidth(4, 1000)
-    """
+"""
