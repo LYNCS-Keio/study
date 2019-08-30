52c760bd525af62ad4a6a08d4b615db6d8beb748
RyosukeSasaki <rsasaki@keio.jp>
2019-08-30 11:59:23 +0100

    add debug

--- DS4_controll/ds4_servo.py
+++ DS4_controll/ds4_servo.py
@@ -1,14 +1,28 @@
 import pygame
 import pigpio
+import time
+
+pygame.init()
 pi=pigpio.pi()
 pygame.joystick.init()
 
-joystick = pygame.joystick.Joystick(0)
+while True:
+    if pygame.joystick.get_count() < 1:
+        print("wait for connect")
+        print(pygame.joystick.get_count())
+        pygame.joystick.quit()
+        time.sleep(0.1)
+    else:
+        print("connected")
+        joystick = pygame.joystick.Joystick(0)
+        break
+
 joystick.init()
 
-
 while True:
+    time.sleep(0.01)
     updown = joystick.get_axis(0)
+    print(updown)
     if updown == 0 :
         pi.set_servo_pulsewidth(4, 1500)
     elif updown > 0 :
