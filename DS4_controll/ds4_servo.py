4f6f3397a5597ef5c76c1caa9fa4bbf6065e4d0c
Ryosuke <rsasaki@keio.jp>
2019-08-30 20:56:47 +0900

    fix

--- DS4_controll/ds4_servo.py
+++ DS4_controll/ds4_servo.py
@@ -42,20 +42,5 @@ try:
             print(leftRight)
 
 
-    """
-    while True:
-        time.sleep(0.01)
-        print(joystick.get_axis(0), joystick.get_axis(1),
-            joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5))
-
-        updown = joystick.get_axis(0)
-        print(updown)
-        if updown == 0 :
-            pi.set_servo_pulsewidth(4, 1500)
-        elif updown > 0 :
-            pi.set_servo_pulsewidth(4, 2000)
-        elif updown < 0 :
-            pi.set_servo_pulsewidth(4, 1000)
-    """
 finally:
     sys.exit()
