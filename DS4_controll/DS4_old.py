56a175e1b7055319880b3d240315f383c6154ed1
ozyoze <hawkenmuryou@gmail.com>
2019-08-30 21:41:35 +0900

    old_ver2

--- DS4_controll/DS4_old.py
+++ DS4_controll/DS4_old.py
@@ -1,33 +1,5 @@
-import ctypes
-test=ctypes.CDLL('./sendSPI.so')
 
 import pygame
-# Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-# This is a simple class that will help us print to the screen
-# It has nothing to do with the joysticks, just outputting the
-# information.
-class TextPrint:
-    def __init__(self):
-        self.reset()
-        self.font = pygame.font.Font(None, 20)
-    def prin(self, screen, textString):
-        textBitmap = self.font.render(textString, True, BLACK)
-        screen.blit(textBitmap, [self.x, self.y])
-        self.y += self.line_height
-
-    def reset(self):
-        self.x = 10
-        self.y = 10
-        self.line_height = 15
-
-    def indent(self):
-        self.x += 10
-
-    def unindent(self):
-        self.x -= 10
-
 pygame.init()
 
 # Set the width and height of the screen [width,height]
@@ -35,12 +7,11 @@ pygame.init()
 #Loop until the user clicks the close button.
 done = False
 # Used to manage how fast the screen updates
-clock = pygame.time.Clock()
+
 # Initialize the joysticks
 pygame.joystick.init()
 
-# Get ready to print
-textPrint = TextPrint()
+
 # -------- Main Program Loop -----------
 while done==False:
     # EVENT PROCESSING STEP
@@ -88,8 +59,7 @@ while done==False:
 
 
 
-    test.trns(axis2,axis2,axis5,button)
-    clock.tick(300)
+    print (axis2,axis2,axis5,button)
 
 # Close the window and quit.
 # If you forget this line, the program will 'hang'
