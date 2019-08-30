e444dfdd09a19d53a74fb19259617e84c1947344
ozyoze <hawkenmuryou@gmail.com>
2019-08-30 17:35:46 +0900

    finally追加

--- LED_pika/LED_pika.py
+++ LED_pika/LED_pika.py
@@ -4,9 +4,13 @@ import pigpio
 pi = pigpio.pi()
 pi.set_mode(18, pigpio.OUTPUT)
 
-while True :
-    pi.write(18, 1)
-    time.sleep(0.5)
+try:
+    while True :
+        pi.write(18, 1)
+        time.sleep(0.5)
+        pi.write(18, 0)
+        time.sleep(0.5)
+        
+finally:
     pi.write(18, 0)
-    time.sleep(0.5)
     
