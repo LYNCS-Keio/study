94477e97f2fb1413728547a62aa0e45a2027c48b
RyosukeSasaki <rsasaki@keio.jp>
2019-08-30 12:16:42 +0100

    あとできれいにします

--- DS4_controll/search_js.py
+++ DS4_controll/search_js.py
@@ -5,9 +5,12 @@ pygame.joystick.init()
 
 js_num = pygame.joystick.get_count()
 
+joystick = pygame.joystick.Joystick(0)
+print(joystick.get_name())
 if js_num != 0:
     for index in range(js_num - 1):
-        joystick = pygame.joystick.Joystick(index)
-        print(joystick.get_name(index))
+        print(pygame.joystick.Joystick.get_name(0))
+        print("AAAA")
 else:
     print("no js found")
+
