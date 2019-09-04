07249086396d4b18ff304b72e90f007ccec9a73d
shun-nagayama <allandarp.shun@gmail.com>
2019-09-04 17:25:05 +0900

    test-of-reply

--- raspberry_pi/bot.py
+++ raspberry_pi/bot.py
@@ -1,5 +1,10 @@
 from slackbot.bot import Bot
 
-#slackbot起動
-raspi_reply_bot = Bot()
-raspi_reply_bot.run()
+#slackbot
+def main():
+    bot = Bot()
+    bot.run()
+
+if __name__ == "__main__":
+    print('start slackbot')
+    main()
